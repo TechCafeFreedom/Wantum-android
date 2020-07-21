86f4e8f8113cb8f5cef773d4e416bc4335883500
Daishin16 <sakuya1202@icloud.com>
2020-07-22 01:20:32 +0900

    init bottomnav

--- app/src/main/java/com/techcafe/wantum/MainActivity.kt
+++ app/src/main/java/com/techcafe/wantum/MainActivity.kt
@@ -3,6 +3,8 @@ package com.techcafe.wantum
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(bottom_navigation_view, navController)
+    }
 }
