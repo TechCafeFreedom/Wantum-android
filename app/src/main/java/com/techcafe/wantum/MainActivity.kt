3c3df716334160b31ae4a1e2a05800e447b0dfd2
Daishin16 <sakuya1202@icloud.com>
2020-07-22 00:44:08 +0900

    init navigation

--- app/src/main/java/com/techcafe/wantum/MainActivity.kt
+++ app/src/main/java/com/techcafe/wantum/MainActivity.kt
@@ -2,11 +2,14 @@ package com.techcafe.wantum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        }
 }
