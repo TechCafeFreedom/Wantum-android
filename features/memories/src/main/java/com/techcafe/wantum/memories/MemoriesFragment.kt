824ef24e73ff0035bd99c9e0440aa5d4ea66eb9e
Daishin16 <sakuya1202@icloud.com>
2020-07-24 15:57:11 +0900

    app / add module(build.gradle-> implementation project)

--- features/memories/src/main/java/com/techcafe/wantum/memories/MemoriesFragment.kt
+++ features/memories/src/main/java/com/techcafe/wantum/memories/MemoriesFragment.kt
@@ -1,9 +1,8 @@
-package com.techcafe.wantum.memories.java
+package com.techcafe.wantum.memories
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import com.techcafe.wantum.memories.R
 
 class MemoriesFragment : Fragment(R.layout.fragment_memories) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
