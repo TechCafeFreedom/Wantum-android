945699570953bfed3706d2b92b76f2454eeee3e4
batch <baaaakkrad@gmail.com>
2020-08-19 21:19:08 +0900

    hide bottomnav

--- app/src/main/java/com/techcafe/wantum/MainActivity.kt
+++ app/src/main/java/com/techcafe/wantum/MainActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.setupWithNavController
@@ -21,8 +22,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // TODO AuthFragmentからの遷移
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 //        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
 //        supportFragmentManager.beginTransaction().also {
 //            it.replace(binding.navHostFragment.id, SignUpFragment.newInstance())
@@ -31,7 +33,14 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        setupWithNavController(bottom_navigation_view, navController)
+        setupWithNavController(binding.bottomNavigationView, navController)
 //        }
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNavigationView.isVisible = when(destination.id) {
+                R.id.wannado_detail -> false
+                else -> true
+            }
+        }
     }
 }
