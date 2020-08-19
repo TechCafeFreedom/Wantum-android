dd35b0137d5949aaaa03a5fe45f3ba1c36aec067
batch <baaaakkrad@gmail.com>
2020-08-19 21:26:30 +0900

    remove no need code

--- app/src/main/java/com/techcafe/wantum/MainActivity.kt
+++ app/src/main/java/com/techcafe/wantum/MainActivity.kt
@@ -1,18 +1,13 @@
 package com.techcafe.wantum
 
 import android.os.Bundle
-import android.view.View
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.setupWithNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.techcafe.wantum.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -24,15 +19,10 @@ class MainActivity : AppCompatActivity() {
         // TODO AuthFragmentからの遷移
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-//        supportFragmentManager.beginTransaction().also {
-//            it.replace(binding.navHostFragment.id, SignUpFragment.newInstance())
-//            it.commitNow()
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         setupWithNavController(binding.bottomNavigationView, navController)
-//        }
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             binding.bottomNavigationView.isVisible = when(destination.id) {
