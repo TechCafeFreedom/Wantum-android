1bb63386b259b1380c8caa2ced1d157e0542fcb0
batch <baaaakkrad@gmail.com>
2020-09-07 14:34:47 +0900

    remove share button from toolbar

--- features/resource/src/main/java/com/techcafe/wantum/resource/OnClickToolbarItemListener.kt
+++ features/resource/src/main/java/com/techcafe/wantum/resource/OnClickToolbarItemListener.kt
@@ -3,5 +3,4 @@ package com.techcafe.wantum.resource
 interface OnClickToolbarItemListener {
     fun onClickBack()
     fun onClickMore()
-    fun onClickShare()
 }
--- features/wannado/src/main/java/com/techcafe/wantum/wannado/WannadoDetailFragment.kt
+++ features/wannado/src/main/java/com/techcafe/wantum/wannado/WannadoDetailFragment.kt
@@ -1,16 +1,14 @@
 package com.techcafe.wantum.wannado
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.techcafe.wantum.resource.OnClickToolbarItemListener
 import com.techcafe.wantum.wannado.databinding.FragmentWannadoDetailBinding
 
-class WannadoDetailFragment : Fragment(), OnClickToolbarItemListener {
+class WannadoDetailFragment : Fragment(R.layout.fragment_wannado_detail), OnClickToolbarItemListener {
 
     private lateinit var binding: FragmentWannadoDetailBinding
     private val args: WannadoDetailFragmentArgs by navArgs()
@@ -23,21 +21,10 @@ class WannadoDetailFragment : Fragment(), OnClickToolbarItemListener {
         // TODO Moreボタンタップされたときの処理
     }
 
-    override fun onClickShare() {
-        // TODO Shareボタンタップされたときの処理
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentWannadoDetailBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentWannadoDetailBinding.bind(view)
+
         binding.toolbarChelWannadoDetail.title = args.wannado
         binding.toolbarChelWannadoDetail.isShowMore = true
         binding.toolbarChelWannadoDetail.isShowShare = true
